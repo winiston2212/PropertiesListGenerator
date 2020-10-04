6af7d38871016694fb85dee7d1059bee3db683b8
null-val <59544401+null-val@users.noreply.github.com>
2020-10-03 18:30:58 -0700

    Added save capability

--- Program.cs
+++ Program.cs
@@ -10,21 +10,76 @@ namespace PropertiesListGenerator
     class Program
     {
         private static readonly List<string> Properties = new List<string>() {
-            
+            //GPS
+            "System.GPS.Latitude",
+            "System.GPS.LatitudeRef",
+            "System.GPS.Longitude",
+            "System.GPS.LongitudeRef",
+            "System.GPS.Altitude",
+
+            //Photo
+            "System.Photo.ExposureTime",
+            "System.Photo.FocalLength",
+            "System.Photo.Aperture",
+            "System.Photo.DateTaken",
+
+            //Audio
+            "System.Audio.ChannelCount",
+            "System.Audio.EncodingBitrate",
+            "System.Audio.Compression",
+            "System.Audio.Format",
+            "System.Audio.SampleRate",
+
+            //Music
+            "System.Music.AlbumID",
+            "System.Music.DisplayArtist",
+            "System.Media.CreatorApplication",
+
         }; 
 
         private static readonly Dictionary<string, PropertyType> Types = new Dictionary<string, PropertyType>() {
-            {"uint32", PropertyType.UInt32},
-            {"string", PropertyType.String},
+            {"UInt32", PropertyType.UInt32},
+            {"String", PropertyType.String},
+            {"Multivalue String", PropertyType.MultivalueString},
+            {"Double", PropertyType.Double},
+            {"DateTime", PropertyType.DateTime}
 
         };
 
+        private static string FilePath = "C:\\Users\\winis\\Documents\\FilePropertyList.cs";
         static void Main(string[] args)
         {
             GetTypeFromDocs("System.AppZoneIdentifier");
+            var propertiesList = new List<PropertyItem>();
+            foreach (var item in Properties)
+            {
+                var prop = new PropertyItem();
+                prop.Path = item;
+                prop.Name = GetPropertyName(item);
+                propertiesList.Add(prop);
+            }
+
+            SaveData(propertiesList);
         }
 
-        private  static string GetPropertyName(string path) {
+        private static void SaveData(List<PropertyItem> items) {
+            StreamWriter writer = File.CreateText(FilePath);
+            writer.WriteLine("using System; \nusing System.Collections.Generic;");
+            writer.WriteLine("class PropertiesList {");
+            writer.WriteLine("\tpublic List<PropertiesListItem> Properties = new List<PropertiesListItem>() {");
+            foreach (var item in items)
+            {
+                writer.WriteLine("\t\t" + "new PropertyListItem() {");
+                writer.WriteLine(string.Format("{0}Name = \"{1}\",", "\t\t\t", item.Name));
+                writer.WriteLine(string.Format("{0}Path = \"{1}\",", "\t\t\t", item.Path));
+                writer.WriteLine("\t\t" + "},");
+            }
+            writer.WriteLine("\t};");
+            writer.WriteLine("}");
+
+            writer.Close();
+        } 
+        private static string GetPropertyName(string path) {
             var array = path.Split('.');
             var name = array[array.Length-1];
             name = string.Concat(name.Select(x => Char.IsUpper(x) ? " " + x : x.ToString())).TrimStart(' ');
@@ -55,6 +110,23 @@ namespace PropertiesListGenerator
             UInt32,
             String,
             MultivalueString,
+            Double,
+            DateTime,
         }
+
+        private class PropertyItem {
+            public PropertyType Type {get; set;}
+            public string Name {get; set;}
+            public string Path {get; set;}
+            public string ConverterName {get; set;}
+            public string ControlType {get; set;}
+            public string Section {get; set;}
+            public bool IsReadOnly {get; set;}
+            
+            public PropertyItem() {
+
+            }
+        }
+
     }
 }
