9ba262687be5683a91c3d081db8c0bd3df07013d
null-val <59544401+null-val@users.noreply.github.com>
2020-11-18 21:50:36 -0800

    Updated for new version of code

--- Program.cs
+++ Program.cs
@@ -4,12 +4,36 @@ using System.Linq;
 using System.IO;
 using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace PropertiesListGenerator
 {
     class Program
     {
         private static readonly List<string> Properties = new List<string>() {
+            //Core
+            "System.RatingText",
+            "System.ItemFolderPathDisplay",
+            "System.ItemTypeText",
+            "System.Title",
+            "System.Subject",
+            "System.Comment",
+            "System.Copyright",
+            "System.DateCreated",
+            "System.DateModified",
+
+            //Image
+            "System.Image.ImageID",
+            "System.Image.CompressedBitsPerPixel",
+            "System.Image.BitDepth",
+            "System.Image.Dimensions",
+            "System.Image.HorizontalResolution",
+            "System.Image.VerticalResolution",
+            "System.Image.CompressionText",
+            "System.Image.ResolutionUnit",
+            "System.Image.HorizontalSize",
+            "System.Image.VerticalSize",
+
             //GPS
             "System.GPS.Latitude",
             "System.GPS.LatitudeRef",
@@ -18,6 +42,8 @@ namespace PropertiesListGenerator
             "System.GPS.Altitude",
 
             //Photo
+            "System.Photo.CameraManufacturer",
+            "System.Photo.CameraModel",
             "System.Photo.ExposureTime",
             "System.Photo.FocalLength",
             "System.Photo.Aperture",
@@ -34,19 +60,52 @@ namespace PropertiesListGenerator
             "System.Music.AlbumID",
             "System.Music.DisplayArtist",
             "System.Media.CreatorApplication",
+            "System.Music.AlbumArtist",
+            "System.Music.AlbumTitle",
+            "System.Music.Artist",
+            "System.Music.BeatsPerMinute",
+            "System.Music.Composer",
+            "System.Music.Conductor",
+            "System.Music.DiscNumber",
+            "System.Music.Genre",
+            "System.Music.TrackNumber",
+
+            //Media
+            "System.Media.AverageLevel",
+            "System.Media.Duration",
+            "System.Media.FrameCount",
+            "System.Media.ProtectionType",
+
+            //Media
+            "System.Media.AuthorUrl",
+            "System.Media.ContentDistributor",
+            "System.Media.DateReleased",
+            "System.Media.SeriesName",
+            "System.Media.SeasonNumber",
+            "System.Media.EpisodeNumber",
+            "System.Media.Producer",
+            "System.Media.PromotionUrl",
+            "System.Media.ProviderStyle",
+            "System.Media.Publisher",
+            "System.Media.ThumbnailLargePath",
+            "System.Media.ThumbnailLargeUri",
+            "System.Media.ThumbnailSmallPath",
+            "System.Media.ThumbnailSmallUri",
+            "System.Media.UniqueFileIdentifier",
+            "System.Media.UserWebUrl",
+            "System.Media.Writer",
+            "System.Media.Year",
+        };
+
+        private static readonly Dictionary<string, string> Converters = new Dictionary<string, string>() {
+            {"UInt32", "new UInt32ToString()"},
+            {"Multivalue String", "new StringArrayToString()"},
+            {"Double", "new DoubleToString()"},
+            {"DateTime", "new DateTimeOffsetToString()"}
 
         };
 
-        private static readonly Dictionary<string, PropertyType> Types = new Dictionary<string, PropertyType>() {
-            {"UInt32", PropertyType.UInt32},
-            {"String", PropertyType.String},
-            {"Multivalue String", PropertyType.MultivalueString},
-            {"Double", PropertyType.Double},
-            {"DateTime", PropertyType.DateTime}
-
-        };
-
-        private static string FilePath = "C:\\Users\\winis\\Documents\\FilePropertyList.cs";
+        private static string FilePath = "C:\\Users\\winis\\OneDrive\\Programming\\GitHub\\PropertiesListGenerator\\bin\\FilePropertyList.cs";
         static void Main(string[] args)
         {
             var propertiesList = new List<PropertyItem>();
@@ -67,11 +126,12 @@ namespace PropertiesListGenerator
             writer.WriteLine("\tpublic List<PropertiesListItem> Properties = new List<PropertiesListItem>() {");
             foreach (var item in items)
             {
-                writer.WriteLine("\t\t" + "new PropertyListItem() {");
+                writer.WriteLine("\t\t" + "new FileProperty() {");
                 writer.WriteLine(string.Format("{0}Name = \"{1}\",", "\t\t\t", item.Name));
-                writer.WriteLine(string.Format("{0}Path = \"{1}\",", "\t\t\t", item.Path));
+                writer.WriteLine(string.Format("{0}Property = \"{1}\",", "\t\t\t", item.Path));
                 writer.WriteLine(string.Format("{0}Section = \"{1}\",", "\t\t\t", item.Section));
-                writer.WriteLine(string.Format("{0}Type = \"{1}\",", "\t\t\t", item.Type));
+                if(!string.IsNullOrWhiteSpace(item.ConverterName))
+                    writer.WriteLine(string.Format("{0}Converter = \"{1}\",", "\t\t\t", item.ConverterName));
                 writer.WriteLine("\t\t" + "},");
             }
             writer.WriteLine("\t};");
@@ -123,23 +183,20 @@ namespace PropertiesListGenerator
                 var url = "https://docs.microsoft.com/en-us/windows/win32/properties/props-" + Path.Replace('.', '-').ToLower();
                 var data = new System.Net.WebClient().DownloadString(url);
                 var type = data.Split("type = ")[1].Split("\n")[0];
-                Console.WriteLine(type);
-                Type = GetType(type);
+                ConverterName = GetConverter(type: type);
             }
 
             private void SetSection() {
                 Section = Path.Split('.')[1];
             }
 
-            private static PropertyType GetType(string type)
-            {
-                foreach (var item in Types)
+            private string GetConverter(string type) {
+                foreach (var item in Converters)
                 {
-                    if (item.Key.ToLower().Contains(type.ToLower()))
+                    if (item.Key.Trim().ToLower().Equals(type.ToLower()))
                         return item.Value;
                 }
-
-                return PropertyType.String;
+                return null;
             }
         }
 
